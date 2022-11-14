36e08a180503ad2958ba76293648b53e46d03ee2
esposito.00@outlook.it <esposito.00@outlook.it>
2022-11-14 15:33:47 +0100

    for/while

---
+++ logic.js
@@ -0,0 +1,21 @@
+// Lista della spesa con ciclo while
+
+
+// data una lista della spesa, stampare sulla pagina gli elementi della lista individualmente con un ciclo for (da commentare)
+
+// fare lo stesso con il while (da lasciare attivo)
+
+
+// creare lista spesa
+const foodList = ['pane', 'yogurt', 'pasta', 'uova', 'latte'];
+
+// for (let i = 0; i < foodList.length; i++){
+    // console.log(foodList[i]);
+// }
+
+let i = 0;
+while (i < foodList.length){
+    console.log(foodList[i]);
+    i++;
+}
+
